d99ce9cfe5f53ef204df5b8e5f22046b92937cca
Dibakar <iamdibakardipu@gmail.com>
2023-02-07 23:35:00 +0600

    Delete valid-palindrom directory

--- valid-palindrom/isPalindromeSimple.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isPalindrome = function(s) {
-    let string = s.toLowerCase().replace(/[^A-Za-z0-9]/g, '')
-    return string === string.split('').reverse().join('') ? true : false
-};
